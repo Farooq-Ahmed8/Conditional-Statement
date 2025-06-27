98930b8a54c0f7f9fb170e8b8fa78a36d52e9da1
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-27 08:32:38 -0700

    conditiona; statement completed

---
+++ app.js
@@ -0,0 +1,75 @@
+//JavaScript mein conditions ka matlab hota hai ase statements jo kisi condition 
+//(shart) ke upar kaam karte hain — agar condition true hai toh kuch code chale,
+//  aur agar false hai toh kuch aur ho.
+//Yeh programming ka basic concept hai, jise decision making bhi kehte hain.
+
+
+//Agar condition true hai, tabhi code chalega.
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("App vote dy sakte hain");
+// }
+
+// Condition false ho toh doosra code chalega.
+
+// let age = 16
+
+// if(age >= 18){
+//     console.log("App vote de sakte hain");
+// }else{
+//     console.log("App vote nhi de sakte");
+    
+// }
+
+
+// Multiple conditions check karne ke liye.
+
+// let marks = 75;
+// if(marks >= 90){
+//     console.log("Grade A");    
+// }else if(marks >= 70){
+//     console.log("Grade B");    
+// }else{
+//     console.log("Grade C");
+// }
+
+//Check the Student Result Marsheet
+
+// let num = +prompt("Enter Your Marks");
+// let grad;
+
+// if(num >= 96 && num <= 100){
+//     grad = "A+";
+//     console.log("Your Grade is ", grad);    
+// }else if(num >= 90 && num <= 95){
+//     grad = "A";
+//     console.log("Your Grade is ", grad);    
+// }else if(num >= 80 && num <= 89){
+//     grad = "B";
+//     console.log("Your Grade is ", grad);    
+// }else if(num >= 70 && num <= 79){
+//     grad = "C";
+//     console.log("Your Grade is ", grad);    
+// }else if(num >= 60 && num <= 69){
+//     grad = "D";
+//     console.log("Your Grade is ", grad);    
+// }else if(num >= 50 && num <= 59){
+//     grad = "E";
+//     console.log("Your Grade is ", grad);    
+// }else if(num >= 10 && num < 50){
+//     grad = "Fail";
+//     console.log("Your are ", grad);
+// }else{ 
+//     console.log("Invalid number") 
+// }
+
+
+//Ek line mein condition check karne ke liye.
+
+// let age = 20;
+// let result = (age >= 18) ? "Adult": "UnAdult"
+
+// console.log(result);   //"Adult"
+
